2aa1a2db41021f7f3384fa2650e51ad81a89f54e
86-Ashik <nusayerashik@iut-dhaka.edu>
2020-03-13 16:01:30 +0600

    Test

--- DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem3Test.cs
+++ DesignPatterns/DesignPatterns.Test/MidSemester/MidProblem3Test.cs
@@ -8,12 +8,13 @@ namespace DesignPatterns.Test.MidSemester
         [Fact]
         void DoTest()
         {
-            Preference instance1 = null; // initialize correctly
+            Preference instance1 = Preference.getInstance(); // initialize correctly
             // set value "123" with key "abc"
-            string abcValue = null; // get with key "abc";
+            instance1.setPreferance("123", "abc");
+            string abcValue = instance1.getPreferance("abc"); // get with key "abc";
             Assert.Equal("123", abcValue);
 
-            Preference instance2 = null; // initialize correctly
+            Preference instance2 = Preference.setPreferance(); // initialize correctly
             // set value "567" with key "abc" in instance2.
             // get value with key "abc" from instance1.
             // assert
